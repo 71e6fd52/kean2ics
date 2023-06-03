cbebf079317bf4fd5412709d70f41856b9205a7a
71e6fd52 <71e6fd52@gmail.com>
2023-06-03 19:47:53 +0800

    fix: delete unused route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'schedule/login'
   post 'schedule/login', to: 'schedule#perform_login'
   get 'schedule/choose'
-  post 'schedule/choose', to: 'schedule#choose_post'
   get 'schedule/logout'
   get 'schedule/generate/:term', to: 'schedule#generate'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
